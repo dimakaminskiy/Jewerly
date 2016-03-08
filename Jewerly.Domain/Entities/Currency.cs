4187ecd10b1d846bf2a4cf7e66556db6de01b2d6
HelenS93 <Elena.Odessa11@gmail.com>
2016-03-08 18:32:45 +0200

    admin html

--- Jewerly.Domain/Entities/Currency.cs
+++ Jewerly.Domain/Entities/Currency.cs
@@ -1,19 +1,28 @@
-﻿namespace Jewerly.Domain
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Jewerly.Domain
 {
     public class Currency
     {
         public int CurrencyId { get; set; }
+        [Display(Name = "Название")]
         public string Name { get; set; }
+        [Display(Name = "Код валюты")]
         public string CurrencyCode { get; set; }
+        [Display(Name = "Соотношение")]
         public decimal Rate { get; set; }
+        [Display(Name = "Опубликовано?")]
         public bool Published { get; set; }
+        [Display(Name = "Порядок сортировки")]
         public int DisplayOrder { get; set; }
     }
 
     public class Discount
     {
         public int Id { get; set; }
+        [Display(Name = "Название")]
         public string Name { get; set; }
+        [Display(Name = "Значение")]
         public int Value { get; set; }
        
     }
@@ -21,8 +30,11 @@
     public class Markup // наценка
     {
         public int Id { get; set; }
+        [Display(Name = "Название")]
         public string Name { get; set; }
+        [Display(Name = "Розничная")]
         public int Retail { get; set; }
+        [Display(Name = "Оптовая")]
         public int Trade { get; set; }
        
     }
