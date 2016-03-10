33c920f8c41e80ab896ee6a4ac11fbe602e03f73
HelenS93 <Elena.Odessa11@gmail.com>
2016-03-10 17:01:02 +0200

    Countries/Index.cshtml

--- Jewerly.Domain/Db/ApplicationUser.cs
+++ Jewerly.Domain/Db/ApplicationUser.cs
@@ -1,4 +1,5 @@
-﻿using System.Security.Claims;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Jewerly.Domain.Entities;
 using Microsoft.AspNet.Identity;
@@ -15,14 +16,21 @@ namespace Jewerly.Domain
             // Здесь добавьте утверждения пользователя
             return userIdentity;
         }
-      
+        [Display(Name = "Фамилия")]
         public string LastName { get; set; }
+        [Display(Name = "Имя")]
         public string FirstName { get; set; }
+        [Display(Name = "Отчество")]
         public string MiddleName { get; set; }
+        [Display(Name = "Страна")]
         public int CountryId { get; set; }
+        [Display(Name = "Валюта")]
         public int CurrencyId { get; set; }
+        [Display(Name = "Город")]
         public string City { get; set; }
+        [Display(Name = "Вид деятельности")]
         public string KindOfActivity { get; set; }
+
 
         public virtual Country Country { get; set; }
         public virtual Currency Currency { get; set; }
--- Jewerly.Domain/Entities/Country.cs
+++ Jewerly.Domain/Entities/Country.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ namespace Jewerly.Domain.Entities
    public class Country
     {
         public int Id { get; set; }
+        [Display(Name = "Название")]
         public string Name { get; set; }
     }
 }
